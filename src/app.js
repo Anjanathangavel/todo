b2b0c3dc550f1dd503c9ed8ac3a1fedb2a213089
Anjana <anjanathangavel05@gmail.com>
2025-04-13 15:39:31 +0530

    second commit

--- src/app.js
+++ src/app.js
@@ -2,7 +2,8 @@ const express =require('express');
 const todoRouter =require('./routes/todo.js');
 const app=express();
 const port=3000;
-
+    
+app.use(express.json());
 app.use('/todo',todoRouter);
 
 
