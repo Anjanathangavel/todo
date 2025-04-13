292ae61a71bf938a11ba1177a2872aef171963fe
Anjana <anjanathangavel05@gmail.com>
2025-04-13 14:56:44 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express =require('express');
+const todoRouter =require('./routes/todo.js');
+const app=express();
+const port=3000;
+
+app.use('/todo',todoRouter);
+
+
+app.listen(port,()=>{
+    console.log(`Server is running in port`,port);
+});
